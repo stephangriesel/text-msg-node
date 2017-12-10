ad35f4b7e1549fe97d3282d52ca8d174029e7620
Stephan Griesel <sgriesel@gmail.com>
2017-12-10 17:41:53 +0100

    implement render view

---
+++ script.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const Nexmo = require('nexmo');
+const socketio = require('socket.io');
+
+// init app 
+const app = express();
+
+//template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+//public folder setup
+app.use(express.static(__dirname + '/public'));
+
+// body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/', (req,res) => {
+    res.render('index');
+})
+
+// define port
+const port = 3000;
+
+// start server
+
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
